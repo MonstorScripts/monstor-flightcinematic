e2a5d9f71cf311fdcaa63174a67c5a6ff6c4517e
Alivemonstor <alivemonstor55@gmail.com>
2026-08-18 14:12:19 +0100

    changed: IsCarryingAnotherPlayer now reuses the attached-player scan instead of duplicating its own nearby-player loop.

--- config.lua
+++ config.lua
@@ -11,6 +11,8 @@ Config.MaxCashAllowance = 10000
 
 Config.BoardingDistance = 8.0
 
+Config.AttachedScanDistance = 5.0
+
 Config.Jobs = {
     police = { keepItems = true, freeFare = true },
     doj = { keepItems = true, freeFare = true },
